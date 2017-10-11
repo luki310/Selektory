8ee468aad78a82e025c4da8389d0d01800606c5d
Łukasz Kuczek <lkuczek@wp.pl>
2017-10-11 04:13:52 +0200

    Added all HTML

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 (function($) {
     $(document).ready(function() {
-        var grid = $("div.grid.grid-12");
-        console.log(grid.length);
+        var grid = $("div.grid.grid-12"),
+            link = $("nav a[href^='http']"),
+            paragraf = $("div#text p:not(:has('*')):first");
+        console.log(paragraf.length);
+        paragraf.fadeTo( 1500, 0.3 );
     });
 })(jQuery);
