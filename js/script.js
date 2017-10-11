1bed5c9dd203adac1ddf000bc5baff8941ac3068
Łukasz Kuczek <lkuczek@wp.pl>
2017-10-11 13:26:11 +0200

    READY

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@
     $(document).ready(function() {
         var grid = $("div.grid.grid-12"),
             link = $("nav a[href^='http']"),
-            paragraf = $("div#text p:not(:has('*')):first");
-        console.log(paragraf.length);
-        paragraf.fadeTo( 1500, 0.3 );
+            inputs = $("input:checkbox[checked!='checked'], input:radio[checked!='checked']"),
+            paragraf = $("div#text p:not(:has('*')):first"),
+            pagination = $(".pagination-item:not('span')");
+
+            grid.addClass("border-on");
+            link.addClass("border-on");
+            inputs.parent().addClass("border-on");
+            paragraf.addClass("border-on");
+            pagination.addClass("border-on");
     });
 })(jQuery);
